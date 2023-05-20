f45988df89aabe7af6cd14979882f343562adae7
Samaya <samaya2024@gmail.com>
2023-05-20 12:21:46 -0700

    images working

--- background1.js
+++ background1.js
@@ -1,8 +1,11 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+// canvas.width =  document.body.clientWidth;
+// canvas.height = document.body.clientHeight;
+
+canvas.width =  1024
+canvas.height = 576
 
 const gravity = 1.5;
 
@@ -35,28 +38,63 @@ class Player {
 }
 
 
+
 class Platform {
-    constructor({x, y}) {
+    constructor({x, y, image}) {
+        this.position = {
+            x,
+            y
+        }
+        this.image = image
+
+        this.width = 761
+        this.height = 83
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+class GenericObject {
+    constructor({x, y, image}) {
         this.position = {
             x,
             y
         }
 
-        this.width = 200
-        this.height = 20
+        this.image = image1
+        this.width = 760
+        this.height = 82
     }
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
+const image = new Image()
+image.src = './images/platform.png'
+console.log(image)
+
+const image1 = new Image()
+image1.src = './images/background.png'
+
+const image2 = new Image()
+image2.src = './images/hills.png'
+
+
 const player = new Player()
 const platforms = [new Platform({
-    x: 200, y: 100
+    x: 0, y: 500, image 
 }), new Platform({
-    x: 500, y: 200
-})]
+    x: 761, y: 500, image
+}),
+]
+
+const genericObjects = [
+    new GenericObject({
+        x:0, y:0, image1
+    })
+]
 
 const keys = {
     right: {
@@ -67,30 +105,38 @@ const keys = {
     }
 }
 
+
+
 function animate() {
     requestAnimationFrame(animate)
+    c.fillStyle = 'white'
     c.clearRect(0,0, canvas.width, canvas.height)
-    player.update()
+    genericObjects.forEach(genericObject => {
+        genericObject.draw()
+    })
+
     platforms.forEach(platform => {
         platform.draw()
     })
-
+    player.update()
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
-    } else if(keys.left.pressed && player.position.x > 100) {
+    } 
+    else if(keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5;
-    } else  {
+    } 
+    else  {
         player.velocity.x = 0
         if (keys.right.pressed) {
             platforms.forEach(platform => {
                 platform.position.x -= 5
             })
         }
-        else if (keys.left.pressed) {
-            platforms.forEach(platform => {
-                platform.position.x +=5;
-            })
-        }
+            else if (keys.left.pressed) {
+                platforms.forEach(platform => {
+                    platform.position.x +=5;
+                })
+            }
     }
 
 //platform collisions
