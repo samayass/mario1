d43c38eb273ae3c976179bb124f583c804fc90cd
Samaya <samaya2024@gmail.com>
2023-05-20 09:57:42 -0700

    mario brick image and brick collisions

--- background1.js
+++ background1.js
@@ -34,7 +34,30 @@ class Player {
     }
 }
 
+
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x,
+            y
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
 const player = new Player()
+const platforms = [new Platform({
+    x: 200, y: 100
+}), new Platform({
+    x: 500, y: 200
+})]
+
 const keys = {
     right: {
         pressed:false
@@ -44,15 +67,42 @@ const keys = {
     }
 }
 
-
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
-    } else player.velocity.x =0
+    } else if(keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5;
+    } else  {
+        player.velocity.x = 0
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        }
+        else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x +=5;
+            })
+        }
+    }
+
+//platform collisions
+platforms.forEach(platform => {
+    if(player.position.y + player.height <= platform.position.y 
+        && player.position.y + player.height + player.velocity.y >= platform.position.y
+        && player.position.x + player.width >= platform.position.x
+        && player.position.x <= platform.position.x + platform.width) 
+        {
+        player.velocity.y = 0;
+    }
+})
 
 }
 
